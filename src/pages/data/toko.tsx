1547ecca9b712b16dd57fb576b09cb68dfdfe946
alldone03 <aldanarsenal@gmail.com>
2023-07-11 00:24:48 +0700

    update fetch data namatoko

--- src/pages/data/toko.tsx
+++ src/pages/data/toko.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import MyLayout from "../../component/mylayout"
+import { AuthData } from "../../store/models/authdata";
+import axios from "axios";
 
 
 
@@ -7,13 +9,45 @@ import MyLayout from "../../component/mylayout"
 
 export default function Toko() {
     const [showModalAdd, setShowModalAdd] = useState(false);
+    const [namaToko, setNamaToko] = useState('');
+    const [changeData, setChangeData] = useState(false);
 
+    const userData: AuthData = JSON.parse(localStorage.getItem('authdata') as string);
+
+    const [namaTokoList, setNamaTokoList] = useState([]);
 
 
     useEffect(() => {
-        // When component loads, fill table with data
 
-    }, []);
+        axios.get(import.meta.env.VITE_API_URL + "/namatoko", {
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': `Bearer ${userData.token}`
+            }
+        }).then((response) => {
+            console.log(response.data);
+            setNamaTokoList(response.data.data);
+        })
+    }, [changeData]);
+
+
+
+    function addNamaToko(event: any) {
+        event.preventDefault();
+        const form = new FormData();
+        form.append('namatoko', namaToko);
+        axios.post(import.meta.env.VITE_API_URL + "/namatoko", form, {
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': `Bearer ${userData.token}`
+            }
+        }).then((response) => {
+            setShowModalAdd(!showModalAdd);
+            setChangeData(!changeData);
+
+        })
+
+    }
 
     return (<>
         <MyLayout>
@@ -32,56 +66,42 @@ export default function Toko() {
                             </tr>
                         </thead>
                         <tbody>
-                            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                <td className="px-6 py-4">1</td>
-                                <td className="px-6 py-4">nama toko</td>
-                            </tr>
+                            {namaTokoList.map((item: any, index: number) => {
+                                return (
+                                    <tr key={index} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700" >
+                                        <td className="px-6 py-4">{index + 1}</td>
+                                        <td className="px-6 py-4">{item.namatoko}</td>
+                                    </tr>);
+                            })}
+
 
                         </tbody>
                     </table>
                 </div>
             </div>
+            <div id="authentication-modal" tabIndex={-1} style={{ backgroundColor: "rgb(75,85,99,0.8)" }} className={`${!showModalAdd ? "hidden " : " "} flex items-center justify-center fixed top-0 left-0 right-0 inset-0 z-50  w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full`} >
+                <div className="absolute bg-white rounded-lg shadow dark:bg-gray-700 ">
+                    <button onClick={() => { setShowModalAdd(!showModalAdd); }} type="button" className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="authentication-modal">
+                        <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                        </svg>
+                        <span className="sr-only">Close modal</span>
+                    </button>
+                    <div className="px-6 py-6 lg:px-8">
+                        <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">Add Toko</h3>
+                        <form className="space-y-6" onSubmit={addNamaToko}>
+                            <div>
+                                <label htmlFor="nama_toko" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama Toko</label>
+                                <input onChange={(e) => setNamaToko(e.target.value)} type="nama_toko" name="nama_toko" id="nama_toko" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Joder Ka Dhani" required />
+                            </div>
 
+                            <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create</button>
 
-            <div id="authentication-modal" tabIndex={-1} className={`${showModalAdd ? "hidden" : ""} fixed top-0 left-0 right-0 z-50  w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full`}>
-                <div className="relative w-full max-w-md max-h-full">
-
-                    <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                        <button type="button" className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="authentication-modal">
-                            <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                            </svg>
-                            <span className="sr-only">Close modal</span>
-                        </button>
-                        <div className="px-6 py-6 lg:px-8">
-                            <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">Sign in to our platform</h3>
-                            <form className="space-y-6" action="#">
-                                <div>
-                                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                                    <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required />
-                                </div>
-                                <div>
-                                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-                                    <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
-                                </div>
-                                <div className="flex justify-between">
-                                    <div className="flex items-start">
-                                        <div className="flex items-center h-5">
-                                            <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
-                                        </div>
-                                        <label htmlFor="remember" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
-                                    </div>
-                                    <a href="#" className="text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
-                                </div>
-                                <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
-                                <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
-                                    Not registered? <a href="#" className="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
-                                </div>
-                            </form>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
-        </MyLayout>
+
+        </MyLayout >
     </>);
 }
